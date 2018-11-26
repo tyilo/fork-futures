df61399bb6393bfe47c45a7ffb332a3dc1231dd6
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2018-11-26 21:28:26 +0100

    Add all 3.2+ python versions to setup.py

--- setup.py
+++ setup.py
@@ -120,6 +120,11 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
